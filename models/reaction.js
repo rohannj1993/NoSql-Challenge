862176ee0795aa1bbbda702f763528318c8a8379
rohannj1993 <rohannj1993@gmail.com>
2021-08-24 17:21:58 -0400

    updated server.js

--- models/reaction.js
+++ models/reaction.js
@@ -22,7 +22,7 @@ const reactionSchema = new Schema ({
    
 
 
-}
+},
 {
  toJSON:{
      getters:true
--- server.js
+++ server.js
@@ -1,3 +1,13 @@
-
-
-
+const express = require("express")
+const dataBase = require("./config/connection")
+const routes = require("./routes")
+const PORT = process.env.PORT||3001
+const app =express()
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+// app.use(routes)
+dataBase.once("open",()=>{
+    app.listen(PORT,()=>{
+        console.log("Server is listening!")
+    })
+})
